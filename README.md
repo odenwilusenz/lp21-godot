8924bd4e43ed023ea184030e5ebf0884a6d2c3a5
Samuel Hafen <samuel.hafen@aol.com>
2025-05-06 17:02:27 +0200

    splitting adding art from opengameart

--- README.md
+++ README.md
@@ -2,5 +2,21 @@
 ![Static Badge](https://img.shields.io/badge/Gimp-gray?style=for-the-badge&logo=gimp&logoColor=white)
 
 # lp21-godot
+
 Ein Prgammierkurs als Freifach für SUS im 3.Zyklus des Lehrplan 21.
+
+## Credits
+
+Used the Free asset "A Platformer in the Forest" by Buch <https://opengameart.org/users/buch>
+
+
+
+Used the Free asset "Generic Platformer Tileset (16x16) + Background" by etqws3 <https://opengameart.org/users/etqws3> 
+
+
+
+Used the Free asset "Platformer Baddies" by GrafxKid <https://opengameart.org/users/grafxkid>
+
+
+
 
--- example/example-game/scripts/player.gd
+++ example/example-game/scripts/player.gd
@@ -1,16 +1,29 @@
 extends CharacterBody2D
 
-var gravity = 700
-var jumpForce = 380
-var speed = 250
+var gravity = 1000
+var jumpForce = 250
+var speed = 100
+
+@onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity*delta
-	elif Input.is_action_just_pressed("jump"):
-		velocity.y = -jumpForce
 	
-	var movement = Input.get_axis("left","right")
-	velocity.x = speed*movement
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		_animated_sprite.play("jump")
+		velocity.y = -jumpForce
+	elif Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		_animated_sprite.scale = Vector2(-1,1)
+		_animated_sprite.play("walking")
+		velocity.x = -speed
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		_animated_sprite.scale = Vector2(1,1)
+		_animated_sprite.play("walking")
+		velocity.x = speed
+	else:
+		_animated_sprite.play("idle")
+		velocity.x = 0
+		
 	
 	move_and_slide()
